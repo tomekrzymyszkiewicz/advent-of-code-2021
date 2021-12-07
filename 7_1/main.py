23fb6717ebb48e2250cd3bb3526aa30a49352284
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-12-07 10:33:30 +0100

    done

---
+++ 7_1/main.py
@@ -0,0 +1,16 @@
+file_name = '7_1/input.txt'
+
+crab_list = []
+
+def main():
+    with open(file_name, 'r') as input:
+        line = input.readline()
+        crab_list = [int(num) for num in line.strip('\n').split(',')]
+    pos_fuel = [0 for i in range(max(crab_list))]
+    for i,pos in enumerate(pos_fuel):
+        pos_fuel[i] = sum([abs(crab - i) for crab in crab_list])
+    # print(pos_fuel)
+    return (pos_fuel.index(min(pos_fuel)),min(pos_fuel))
+
+if __name__ == "__main__":
+    print(main())
