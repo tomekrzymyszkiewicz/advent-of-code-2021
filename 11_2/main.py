85a31f0ae58937f7e96a25e25df4ef485f0b3c45
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-12-11 11:12:27 +0100

    done

---
+++ 11_2/main.py
@@ -0,0 +1,47 @@
+file_name = '11_2/input.txt'
+levels = []
+flashed = []
+flash_counter = 0
+
+
+def main(steps):
+    global levels,flashed,flash_counter
+    with open(file_name, 'r') as input:
+        lines = input.readlines()
+        levels = [[int(level) for level in line.strip('\n')] for line in lines]
+    for step in range(steps):
+        levels = [[int(level+1) for level in line] for line in levels]
+        flashed = [[False for level in line] for line in levels]
+        for i, line in enumerate(levels):
+            for j, level in enumerate(line):
+                if level > 9:
+                    flash(i, j)
+        print('Step',step+1)
+        for line in levels:
+            for level in line:
+                print(level,end='')
+            print('')
+        if all(all(level == 0 for level in line) for line in levels):
+            return step+1
+
+def is_in_levels(i,j):
+    return i >= 0 and i < len(levels) and j >= 0 and j < len(levels[0])
+
+
+def flash(i, j):
+    global flash_counter,levels,flashed
+    if flashed[i][j] == True:
+        return
+    flashed[i][j] = True
+    levels[i][j] = 0
+    flash_counter += 1
+    adjacency = [[i-1,j-1],[i-1,j],[i-1,j+1],[i,j+1],[i+1,j+1],[i+1,j],[i+1,j-1],[i,j-1]]
+    for point in adjacency:
+        if is_in_levels(point[0],point[1]) and flashed[point[0]][point[1]] == False:
+            levels[point[0]][point[1]] += 1
+            if levels[point[0]][point[1]] > 9:
+                flash(point[0],point[1])
+
+
+if __name__ == "__main__":
+    print(main(1000))
